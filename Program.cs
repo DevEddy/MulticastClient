8da95fd32c61b7c9515b9a6a843e7d0c38a90d59
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2021-03-23 11:13:46 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MulticastClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Eine Multicast Gruppenadresse zwischen: 224.0.0.0 - 239.255.255.255
+            var destAddr = IPAddress.Parse("224.0.0.100"); 
+            // Multicast port
+            var destPort = 8080;
+            // Time-to-live für das Datagramm, Standard TTL = 1
+            var TTL = 1;
+            // Multicast Socket
+            var sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            // Setze TTL
+            sock.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, TTL);
+
+            try
+            {
+                while (true)
+                {
+                    // Eingabe der Konsole lesen
+                    Console.Write("Schreibe eine Nachricht: ");
+                    var text = Console.ReadLine();
+                    // Kodiere den Text zu einer Bytesequenz
+                    var textBytes = Encoding.ASCII.GetBytes(text);
+                    // Generiere Zielendpunkt
+                    var endPoint = new IPEndPoint(destAddr, destPort);
+                    // Sende die kodierte Nachricht als UDP-Paket
+                    sock.SendTo(textBytes, 0, textBytes.Length, SocketFlags.None, endPoint);
+                }
+            }
+            finally
+            {
+                sock.Close();
+            }
+        }
+    }
+}
